500f5bee02ed7af436f8f8d2132f238972bb1a8e
mohamed <mohamed.eltaier@insight.com>
2025-06-16 15:16:47 +1000

    first commit

---
+++ src/appName/sbx/main.tf
@@ -0,0 +1,27 @@
+/*
+ * # Resource Group Module
+ *
+ * This module creates an empty Resource Group.
+ */
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resourceGroupName
+  location = var.location
+  tags     = var.tags
+
+}
